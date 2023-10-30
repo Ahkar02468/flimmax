70bff9579dfb5cbc5c1f73643b1c5f1c11629272
Ahkar02468 <arkarshwebaw@gmail.com>
2023-10-30 22:44:51 +0630

    display TV Details

--- js/script.js
+++ js/script.js
@@ -73,9 +73,9 @@ async function displayPopularShows(){
 
 async function displayMovieDetails(){
      const movieId = window.location.search.split('=')[1];
-     console.log(movieId);
+     // console.log(movieId);
      const results = await fetchAPIData(`/movie/${movieId}`);
-     console.log(results);
+     // console.log(results);
      const div = document.createElement('div');
      // div.classList.add('details-top');
           div.innerHTML = `
@@ -109,14 +109,14 @@ async function displayMovieDetails(){
             <ul class="list-group">
                ${results.genres.map( genre => `<li>${genre.name}</>`).join('')}
             </ul>
-            <a href="#" target="_blank" class="btn">${results.homepage}</a>
+            <a href="${results.homepage}" target="_blank" class="btn">${results.homepage}</a>
           </div>
         </div>
         <div class="details-bottom">
           <h2>Movie Info</h2>
           <ul>
-            <li><span class="text-secondary">Budget:</span> $${results.budget}</li>
-            <li><span class="text-secondary">Revenue:</span> $${results.revenue}</li>
+            <li><span class="text-secondary">Budget:</span> $${addCommas(results.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommas(results.revenue)}</li>
             <li><span class="text-secondary">Runtime:</span> ${results.runtime} minutes</li>
             <li><span class="text-secondary">Status:</span> ${results.status}</li>
           </ul>
@@ -126,6 +126,76 @@ async function displayMovieDetails(){
           `;
           document.querySelector('#movie-details').appendChild(div);
 }
+
+async function displayTVDetails(){
+     const tvId = window.location.search.split('=')[1];
+     console.log(tvId);
+     const results = await fetchAPIData(`/tv/${tvId}`);
+     console.log(results);
+     const div = document.createElement('div');
+     div.innerHTML = `
+          <div class="details-top">
+          <div>
+          ${
+               results.poster_path ?
+               `<img
+               src="https://www.themoviedb.org/t/p/w500/${results.poster_path}"
+               class="card-img-top"
+               alt="${results.name}"
+               />` :
+               `<img
+               src="../images/no-image.jpg"
+               class="card-img-top"
+               alt="${results.name}"
+               />`
+          }
+          </div>
+          <div>
+          <h2>${results.name}</h2>
+          <p>
+          <i class="fas fa-star text-primary"></i>
+          ${results.vote_average.toFixed(1)} / 10
+          </p>
+          <p class="text-muted">Release Date: ${results.first_air_date}</p>
+          <p>
+          ${results.overview}
+          </p>
+          <h5>Genres</h5>
+          <ul class="list-group">
+          ${results.genres.map( genre => genre.name).join(', ')}
+          </ul>
+          <a href="${results.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+          </div>
+     </div>
+     <div class="details-bottom">
+          <h2>Show Info</h2>
+          <ul>
+          <li><span class="text-secondary">Number Of Episodes:</span> ${results.number_of_episodes}</li>
+          <li>
+          <span class="text-secondary">Last Episode To Air:</span> ${results.last_episode_to_air.name}
+          </li>
+          <li><span class="text-secondary">Status:</span> ${results.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${results.production_companies.map(company => company.name).join(', ')}</div>
+     </div>
+     `;
+     document.querySelector('#show-details').appendChild(div);
+}
+
+function addCommas(number) {
+     // Convert the number to a string.
+     const numberString = number.toString();
+   
+     // Split the string at the decimal point.
+     const [wholeNumber, decimal] = numberString.split(".");
+   
+     // Add commas to the whole number part.
+     const wholeNumberWithCommas = wholeNumber.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+   
+     // Return the formatted number.
+     return `${wholeNumberWithCommas}${decimal ? `.${decimal}` : ""}`;
+   }
 
 // fetch API from themoviedb.org
 async function fetchAPIData(endpoint){
@@ -173,6 +243,7 @@ function init(){
                console.log('Movie Details');
                break;
           case '/tv-details.html':
+               displayTVDetails();
                console.log('TV Details');
                break;
           case '/search.html':
